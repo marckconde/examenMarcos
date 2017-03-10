803471cc630aebee6befd068851128b1a37c2e80
marckconde <miguelchavarria181@gmail.com>
2017-03-10 12:01:21 -0600

    examen
    examen

---
+++ examenMarcos/funciones.js
@@ -0,0 +1,16 @@
+var BacoB= angular.module("BacoB.[]")
+BacoB.controller ("mensaje", function($scope){
+	$scope.cantidad = 1;
+	$scope.cantidad2= 2;
+	$scope.total= 0;
+
+	$scope.total = $scope.cantidad = $scope.cantidad2
+
+	operaciones.controller("suma" , function()
+
+	$scope.cantidad = 3;
+	$scope.cantidad2= 4;
+	$scope.total = 0;
+
+	$scope.total = $scope.cantidad + $scope.cantidad2
+});
